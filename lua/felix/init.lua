4bf18cdde08b2ec2c42399754d17c6ae611bc35e
felix-brockmeier <felix.brockmeier@outlook.com>
2022-12-28 12:47:23 +0100

    finished setup

--- lua/felix/init.lua
+++ lua/felix/init.lua
@@ -1,18 +1,5 @@
 require("felix.remap")
+require("felix.set")
 
-vim.o.relativenumber = true
 
-vim.opt.clipboard = "unnamedplus"
 
-vim.g.clipboard = {
-  name = "win32yank-wsl",
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf"
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --lf",
-    ["*"] = "win32yank.exe -o --lf"
-  },
-  cache_enable = 1,
-}
